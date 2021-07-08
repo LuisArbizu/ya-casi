c9483e9614b5a24e963453dbd0110455157dadc7
LuisArbizu <00206119@uca.edu.sv>
2021-07-07 20:42:19 -0700

    ghbjn

---
+++ examen-final/UI/FormMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppUCA.Context;
+using AppUCA.Entities;
+using AppUCA.Repository;
+using AppUCA.Repository.Services;
+using AppUCA.ViewModels;
+using Microsoft.VisualBasic.ApplicationServices;
+
+namespace AppUCA.UI
+{
+    public partial class FormMain : Form
+    {
+        private InscriptionServices inscriptions;
+        
+        
+        public FormMain()
+        {
+            InitializeComponent();
+
+            inscriptions = new InscriptionServices();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            /*comboBox1.DataSource = Course;
+            comboBox1.ValueMember = "Id";
+            comboBox1.DisplayMember = "Title";*/
+            
+            // insertando las cosas en el data grid view
+
+            dgvInscriptionShow.DataSource = null;
+            var newDs = inscriptions.GetAll();
+            var mappedDS1 = new List<InscriptionVm>();
+            newDs.ForEach(i => mappedDS1.Add(PFMapper.MapInscriptionToInscriptionVm(i)));
+            dgvInscriptionShow.DataSource = mappedDS1;
+            
+            //----
+            
+            dgvCourseShow.DataSource = null;
+            using (var context = new AppGuiContext())
+            {
+                var newDS = context.Courses.ToList();
+                var mappedDS2 = new List<CourseVm>();
+                newDS.ForEach(c => mappedDS2.Add(PFMapper.MapCourseToCourseVm(c)));
+                dgvCourseShow.DataSource = mappedDS2;
+            }
+            //--------------------------------- 
+            
+            WindowState = FormWindowState.Minimized;
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void btnBookAdd_Click(object sender, EventArgs e)
+        {
+            bool confirm =
+                txtCourseTitleAdd.Text.Length < 50 &&
+                txtCourseTopicAdd.Text.Length < 50 &&
+                txtCourseAuthorAdd.Text.Length < 50 &&
+                txtCourseDurationAdd.Text.Length < 50 &&
+                txtCourseModuleAdd.Text.Length < 50 &&
+                txtCourseScheduleAdd.Text.Length < 50;
+                
+
+            if (confirm) {
+                
+                var db = new AppGuiContext();
+                
+                Course nuevo = new Course( txtCourseTitleAdd.Text, Convert.ToInt32(txtCourseModuleAdd.Text), 
+                txtCourseTopicAdd.Text, txtCourseAuthorAdd.Text,txtCourseDurationAdd.Text, txtCourseScheduleAdd.Text );
+                
+                db.Add(nuevo);
+                db.SaveChanges();
+                
+                MessageBox.Show(text: "Curso agregado exitosamente!", caption: "Inscripcion de Cursos UCA",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);   
+                
+            }
+            else
+                MessageBox.Show(text: "Datos no validos!", caption: "Inscripcion de Cursos UCA",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btnInscriptionAdd_Click(object sender, EventArgs e)
+        {
+            bool confirm =
+                txtInscriptionName.Text.Length < 50 &&
+                txtInscriptionMail.Text.Length < 50 &&
+                txtInscriptionPhone.Text.Length < 13;
+                
+            
+            if (confirm) {
+                var db = new AppGuiContext();
+                
+                Inscription nuevo = new Inscription( txtInscriptionName.Text, txtInscriptionMail.Text, 
+                txtInscriptionPhone.Text);
+                
+                db.Add(nuevo);
+                db.SaveChanges();
+                
+                MessageBox.Show(text: "Usuario creado exitosamente!", caption: "Inscripcion de Cursos UCA",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);   
+                
+            }
+            else
+                MessageBox.Show(text: "Datos no validos!", caption: "Inscripcion de Cursos UCA",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
